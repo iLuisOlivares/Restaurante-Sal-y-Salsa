7b22e65ebee6eb1db2918ccb6c7f73e1ac3bb94f
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-11-25 15:24:19 -0500

    validate input in login

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router";
 import UseAuth from "./auth/useAuth";
 import "./login.css";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 export default function Login() {
   const history = useHistory();
@@ -14,13 +16,52 @@ export default function Login() {
   };
 
   const getUser = () => {
-    fetch(
-      `https://restaurante-sal-salsa20211123190304.azurewebsites.net/api/cliente`
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
+    if (userName.length === 0 || password.length === 0) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Llena todos los espacios pendientes",
+      });
+    } else {
+      // console.log("la contraseña es:", password);
+      // console.log("la usuario es:", userName);
+      fetch(
+        `https://restaurante-sal-salsa20211123190304.azurewebsites.net/api/cliente/${userName}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          // console.log(data);
+          let dbUser = data[0].nombre_usuario;
+          let dbPass = data[0].contrasena;
+          const MySwal = withReactContent(Swal);
+
+          if (data.length === 0) {
+            MySwal.fire({
+              icon: "error",
+              title: "Oops...",
+              text: "El usuario no existe",
+              confirmButtonColor: "#3085d6",
+            });
+          } else if (dbUser === userName && dbPass === password) {
+            MySwal.fire({
+              icon: "success",
+              title: "Inicio de sesión éxitoso",
+              confirmButtonColor: "#3085d6",
+            });
+          } else {
+            MySwal.fire({
+              icon: "error",
+              title: "El correo o contraseña es incorrecto",
+              confirmButtonColor: "#3085d6",
+            });
+          }
+        })
+        .catch((err) => console.log(err));
+    }
   };
+
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <section>
@@ -30,15 +71,19 @@ export default function Login() {
           type="name"
           placeholder="Nombre de usuario"
           className="input-formulario"
+          onChange={(e) => setUserName(e.target.value)}
+          required
         />
 
         <input
           type="password"
           placeholder="Contraseña"
           className="input-formulario"
+          onChange={(e) => setPassword(e.target.value)}
+          required
         />
-        <button type="button" onClick={handleLogin} className="btn-r">
-          Entrar
+        <button type="button" onClick={getUser} className="btn-r">
+          Ingresar
         </button>
       </form>
     </section>
